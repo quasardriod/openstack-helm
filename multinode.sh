0c0dd1d55fb3afbdfd936c761c84909098bf54b6
Sumit Kaushal <quasarstack@gmail.com>
2022-10-20 22:01:06 +0530

    Update pkgs list

--- multinode.sh
+++ multinode.sh
@@ -5,12 +5,12 @@
 cd roles && git pull
 cd ../
 
+echo "Running hosts-setup.yml playbook" && sleep 5
 ansible-playbook -i inventory playbooks/hosts-setup.yml
-
 [ $? != 0 ] && echo "Hosts pre-config failed" && exit 1
 
+echo "Running osh-setup.yml playbook" && sleep 5
 ansible-playbook -i inventory playbooks/osh-setup.yml
-
 [ $? != 0 ] && echo "K8s pre-config failed" && exit 1
 
 
