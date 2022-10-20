91c6a3d50148e59b1a75103ac81a271cdead9a75
Sumit Kaushal <quasarstack@gmail.com>
2022-10-20 18:45:56 +0530

    Pulled more tasks from kvm

--- multinode.sh
+++ multinode.sh
@@ -3,13 +3,15 @@
 # Setup template
 
 template(){
-	become_pass=false
+	become_pass=true
 	
 	if [ $become_pass == "true" ];then
 		ask_pass="-K"
 	else
 		ask_pass=""
 	fi
+	cd roles && git pull
+	cd ../
 
 	ansible-playbook -i inventory ubuntu-setup.yml $ask_pass
 }
