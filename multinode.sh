6365764c8752bae40d49faf2f9f1ccf0477b5ffa
Sumit Kaushal <quasarstack@gmail.com>
2022-10-21 11:38:09 +0530

    Updated osh-setup

--- multinode.sh
+++ multinode.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Setup template
+MULTINODE=true
 
 cd roles && git pull
 cd ../
@@ -10,7 +10,7 @@ ansible-playbook -i inventory playbooks/hosts-setup.yml
 [ $? != 0 ] && echo "Hosts pre-config failed" && exit 1
 
 echo "Running osh-setup.yml playbook" && sleep 5
-ansible-playbook -i inventory playbooks/osh-setup.yml
+ansible-playbook -i inventory playbooks/osh-setup.yml -e multinode_deployment=$MULTINODE
 [ $? != 0 ] && echo "K8s pre-config failed" && exit 1
 
 
