b98d50b64af8b4487c49b3349b6260a31b126335
Sumit Kaushal <quasarstack@gmail.com>
2022-11-07 02:49:01 +0530

    Updated Minikube playbook

--- minikube.sh
+++ minikube.sh
@@ -15,7 +15,7 @@ program_control(){
 
 : ${CLEAN_SETUP:="true"}								# if true, cleanup function will be called to delete existing minikune setup
 : ${dns_config:="true"}
-: ${INSTALL_HORIZON:="true"}						# Needed to continue the 
+: ${INSTALL_HORIZON:="true"}						# Needed to continue the
 : ${VALIDATE_UMBRELLA_CONFIG:="true"} 	# Run validate-umbrella-upgrade-config-changes-do-not-update-other-components.sh
 : ${HELM_TESTS:="no"}										# Run helm tests
 : ${CREATE_STACK:="no"}									# Create heat stack to test openstack
@@ -23,7 +23,7 @@ program_control(){
 echo
 echo "--> Following vars will change the course of the deployment:"
 echo
-echo "CLEAN_SETUP: $CLEAN_SETUP      # Delete existing minikube env" 
+echo "CLEAN_SETUP: $CLEAN_SETUP      # Delete existing minikube env"
 echo "dns_config: $dns_config"
 echo "VALIDATE_UMBRELLA_CONFIG: $VALIDATE_UMBRELLA_CONFIG		# Run test to validate umbrella upgrade config"
 echo "HELM_TESTS: $HELM_TESTS        # Validate Openstack deployed services"
@@ -56,8 +56,8 @@ rm -rf openstack-helm
 rm -rf openstack-helm-infra
 }
 
-if [ "$code_download_only" == false ];then 
-	program_control	
+if [ "$code_download_only" == false ];then
+	program_control
 	[ "$CLEAN_SETUP" == "true" ] && cleanup
 else
 	[ ! -d "openstack-helm" ] && git clone "https://review.opendev.org/openstack/openstack-helm"
@@ -70,9 +70,9 @@ fi
 : ${CONTAINER_DISTRO_VERSION:='focal'}
 : ${UPDATE_CALICO_VERSION:="true"}
 
-# Tests take a lot longer but are an 
+# Tests take a lot longer but are an
 # important check before submitting for review
-# RUN_HELM_TESTS='yes' 
+# RUN_HELM_TESTS='yes'
 
 # HOST NETWORKING
 systemctl start systemd-resolved
@@ -92,19 +92,19 @@ cd ~/openstack-helm
 # insert kube DNS as first nameserver instead of entirely overwriting /etc/resolv.conf
 #sed --in-place "/echo 'nameserver/c\  sudo bash -c \"sed -i -e '1inameserver 10.96.0.10' /etc/resolv.conf\"" ../openstack-helm-infra/tools/gate/deploy-k8s.sh
 
-# Tweak $HOME/openstack-helm/tools/deployment/component/common/openstack.sh to install horizon	
+# Tweak $HOME/openstack-helm/tools/deployment/component/common/openstack.sh to install horizon
 if [ "$INSTALL_HORIZON" == "true" ];then
-	
+
 	# Create symlink for horizin in openstack-helm/openstack/charts
 	[ ! -d horizon ] && echo "ERROR! openstack-helm/horizon not found" && exit 1
 	ln -s ../../horizon openstack/charts/horizon
-	
+
 	[ ! -L openstack/charts/horizon ] && [ ! -f openstack/charts/horizon ] && \
 		echo "ERROR! Failed to create horizon symlink in openstack-helm/openstack/charts/horizon" && exit 1
 
 	# Add horizon chart in openstack-helm/openstack/Charts.yaml
 	sed -i '/^description:\sA\schart\sfor\sopenstack\shelm\s*/i - name: horizon\n  repository: file:\/\/..\/horizon\n  version: ">0.1.0"\n  condition: horizon.enabled\n' openstack/Chart.yaml
-	
+
 	# Copy files from openstack-helm/horizon/values_overrides to openstack-helm/openstack/values_overrides/horizon/
 	[ ! -d openstack/values_overrides/horizon ] && mkdir openstack/values_overrides/horizon && cp -rf horizon/values_overrides/* openstack/values_overrides/horizon/
 
@@ -113,11 +113,11 @@ if [ "$INSTALL_HORIZON" == "true" ];then
 
 	# Add export OSH_EXTRA_HELM_ARGS_HORIZON in openstack-helm/tools/deployment/component/common/openstack.sh
 	sed -i -e '/^export\sHELM_CHART_ROOT_PATH=/a : ${OSH_EXTRA_HELM_ARGS_HORIZON:="$(./tools/deployment/common/get-values-overrides.sh horizon subchart)"}' tools/deployment/component/common/openstack.sh
-	
+
 	sed -i '/^helm\supgrade\s--install/a \  ${OSH_EXTRA_HELM_ARGS_HORIZON} \\' tools/deployment/component/common/openstack.sh
 
 	# Tweak openstack-helm/tools/gate/tests/validate-umbrella-upgrade-config-changes-do-not-update-other-components.sh to add horizon
-	echo 'validate_only_expected_application_changes "horizon" "--set horizon.conf.horizon.log.file.level=WARN"' >> tools/gate/tests/validate-umbrella-upgrade-config-changes-do-not-update-other-components.sh 
+	echo 'validate_only_expected_application_changes "horizon" "--set horizon.conf.horizon.log.file.level=WARN"' >> tools/gate/tests/validate-umbrella-upgrade-config-changes-do-not-update-other-components.sh
 fi
 
 
@@ -150,7 +150,7 @@ RUN_HELM_TESTS=$HELM_TESTS ./tools/deployment/component/common/openstack.sh
 # Run test to validate config upgrade
 if [ "$VALIDATE_UMBRELLA_CONFIG" == "true" ];then
 ./tools/gate/tests/validate-umbrella-upgrade-config-changes-do-not-update-other-components.sh
-fi	
+fi
 
 # Test creation of heat stacks
 [ "$CREATE_STACK" == "yes" ] && ./tools/deployment/developer/common/900-use-it.sh
@@ -160,7 +160,7 @@ if [ "$INSTALL_HORIZON" == "true" ];then
 	# Steps to access horizon WebUI
 	HORIZON_INT_SVC_IP=$(kubectl -n openstack describe service horizon-int|awk '/^IP:/{print $2}')
 	[ -z $HORIZON_INT_SVC_IP ] && echo "Error! Failed to install horizon" && exit 1
-	
+
 	echo "--------------------------------------------"
 	echo "HORIZON IP: $HORIZON_INT_SVC_IP"
 	echo
@@ -169,4 +169,3 @@ if [ "$INSTALL_HORIZON" == "true" ];then
 	echo "Linux Example: ip route add ${HORIZON_INT_SVC_IP} via <Openstack_Host_IP>"
 	echo "Windows Example: route add ${HORIZON_INT_SVC_IP} via <Openstack_Host_IP>"
 fi
-
